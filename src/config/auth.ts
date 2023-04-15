e9c55894284dd0f98242313d8c82c9b001c4fcbf
Renan Moreira <renan.bcc@gmail.com>
2023-04-15 15:14:58 -0300

    BUG Não reconhecia variáveis de ambiente

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,7 +1,9 @@
+import 'dotenv/config';
+
 export default {
   secret_token: process.env.SECRET_TOKEN,
   expires_in_token: process.env.EXPIRES_IN_TOKEN,
   secret_refresh_token: process.env.SECRET_REFRESH_TOKEN,
   expires_in_refresh_token: process.env.EXPIRES_IN_REFRESH_TOKEN,
-  expires_refresh_token_days: process.env.EXPIRES_REFRESH_TOKEN_DAYS
+  expires_refresh_token_days: parseInt(process.env.EXPIRES_REFRESH_TOKEN_DAYS)
 }
